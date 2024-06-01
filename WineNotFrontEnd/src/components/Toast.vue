112a3f2a2a782bba08bf41efb1be22c443af2db6
zacmea <zach.mears@gmail.com>
2024-06-01 09:52:56 -0400

    sets up inital state stores

--- WineNotFrontEnd/src/components/Toast.vue
+++ WineNotFrontEnd/src/components/Toast.vue
@@ -1,6 +1,21 @@
+<script setup>
+import {useToastStore} from '@/stores/toast'
+
+const store = useToastStore()
+
+return {
+    store
+}
+</script>
+
+
 <template>
     <div 
         v-if="store.isVisible"
         class="transition ease-in-out duration-500 delay-500 px-6 py-6 fixed top-full right-8 rounded-xl shadow-lg"
+        :class="store.classes"
+    >
+        {{ store.message }}
+    </div>
 
 </template>
