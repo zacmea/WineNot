fef21ab1d49b205c54f521a320e9b6acf3304d9a
zacmea <zach.mears@gmail.com>
2024-06-06 19:08:35 -0400

    adds basic wine details tile

--- WineNotFrontEnd/src/components/WineDetailsTile.vue
+++ WineNotFrontEnd/src/components/WineDetailsTile.vue
@@ -7,10 +7,25 @@ const imageURL = ref('https://i.imgur.com/w398AJB.jpeg');
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center">
-        <img :src="imageURL" alt="Generic wine bottle" class="w-1/2" />
-        <h1 class="text-lg font-bold mt-8">WineName</h1>
-        <p class="mt-2">WineDescription</p>
-        <p class="mt-2">WinePrice</p>
+    <div class="rounded-lg bg-slate-500 p-5 grid-rows-5 rows-5 columns-2 grid-flow-col gap-4">
+
+        <img :src="imageURL" alt="Generic wine bottle" class="rounded-2xl row-span-3" />
+
+        <div id="rightColumn" class="block">
+            <div class="" style="break-before: column; font-weight: bold">
+                <h1 class="text-2xl">WineName</h1>
+            </div>
+            <div class=" mt-5 pt-5">Wine Description</div>
+            <br>
+            <br>
+            <br>
+            <div class="relative mt-20 ml-20 h-20 w-20">
+                <div class="absolute bottom-0 right-0 text-end" style="break-before: row;">Wine price</div>
+            </div>
+        </div>
+        <!-- end of rightColumn -->
     </div>
+
+
+
 </template>
