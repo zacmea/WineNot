2aa5bc38152c555345a04e9b059371773173bfb1
zacmea <zach.mears@gmail.com>
2024-06-08 16:13:32 -0400

    sets up collexn index and collexn details correctly. can see wines in a collexn now.

--- winenot_backend/collexn/serializers.py
+++ winenot_backend/collexn/serializers.py
@@ -7,6 +7,8 @@ class WineSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CollexnSerializer(serializers.ModelSerializer):
+    wines = WineSerializer(many=True, read_only=True)
+    # by default, you'd normally only get back the id of the wine. This sends the id to the serializer and then will give you the full wine object
     class Meta:
         model = Collexn
         fields = '__all__'
