aa79453996e595f5573063911f9b705124878d5c
zacmea <zach.mears@gmail.com>
2024-06-09 12:36:33 -0400

    adds about page and links it to home splash

--- WineNotFrontEnd/src/components/HomeSplash.vue
+++ WineNotFrontEnd/src/components/HomeSplash.vue
@@ -2,17 +2,17 @@
 
     <body >
         <template v-if="userStore.user.isAuthenticated">
-            <div class="bg-blue-300 text-center text-3xl rounded-lg pb-6">
+            <div class="bg-blue-200 text-center text-3xl rounded-lg pb-6">
             <h1 class="py-4">Welcome, {{ userStore.user.first_name }}!</h1>
             <h2 class="text-2xl pb-4">Wanna plan for some wine-ing? Wine not!</h2>
             
             <hr class="border-1">
             <br>
             
-                <RouterLink class="bg-blue-700 rounded-xl px-2 py-1 mt-10" to="/lists">view your wine lists</RouterLink>
+                <RouterLink class=" border-2 border-blue-600 rounded-xl px-2 py-1 mt-10 pb-1.5" to="#">using the app</RouterLink>
                 <br>
                 <br>
-                <RouterLink class="bg-blue-700 rounded-xl px-2 py-1 my-10" to="/search">search & browse wines</RouterLink>
+                <RouterLink class=" border-2 border-blue-600 rounded-xl px-2 py-1 mt-10 pb-1.5" to="/about">about this project</RouterLink>
             </div>
         </template>
         <template v-else>
--- WineNotFrontEnd/src/views/AboutView.vue
+++ WineNotFrontEnd/src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About the developer</h1>
+    <p>Zachary Mears spent a decade in K-12 public education before beginning a transition into tech in 2023.  His time in education was defined by a strong understanding of how people learn and process information and how they navigate the world around them.  As a developer, Zachary brings to bear that same understanding, coupled with an innate curiousity about how things are built and how people interact with the products in their everyday lives.</p>
+
+    <h1>About the project: </h1>
+    <p>This app was built with a combination of Node.js with Tailwind stylings on the front end, and Django/python on the back end.  It connects to a PSQL database housed on Supabase.</p>
+    <p>Wine Not is the final project the developer completed for his (link)full-stack bootcamp at General Assembly.</p>
+    <p>(link)Project repo on github</p>
   </div>
 </template>
