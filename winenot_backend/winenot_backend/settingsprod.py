588e66db7948eb112af1e40c9f248e02f123d57b
zacmea <zach.mears@gmail.com>
2024-06-12 15:43:18 -0400

    deployment attempt 17.1

--- winenot_backend/winenot_backend/settingsprod.py
+++ winenot_backend/winenot_backend/settingsprod.py
@@ -31,7 +31,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['localhost', 'api.winenot.com', 'wine-not.netlify.app']
 
 #Url for the frontend
-WEBSITE_URL = 'wine-not.netlify.app'
+WEBSITE_URL = 'http://localhost:5173'
 
 
 
@@ -186,22 +186,22 @@ USE_TZ = True
 
 
 # HEY RENDER USERS: Uncomment the following SECTION to enable HTTPS on your Render deployment.  To run it locally comment it out.
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# This setting informs Django of the URI path from which your static files will be served to users
-# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-STATIC_URL = 'wine-not.netlify.app/static/'
+# # This setting informs Django of the URI path from which your static files will be served to users
+# # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+# STATIC_URL = 'wine-not.netlify.app/static/'
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
+# # This production code might break development mode, so we check whether we're in DEBUG mode
+# if not DEBUG:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+#     # and renames the files with unique names for each version to support long-term caching
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
