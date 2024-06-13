d291bf18e2409547d6e8ea1918a0fa64a7c8e80a
zacmea <zach.mears@gmail.com>
2024-06-13 12:32:26 -0400

    deploy working. hides secret key

--- winenot_backend/collexn/admin.py
+++ winenot_backend/collexn/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Wine, Collexn
+from .models import Wine, Collexn, User
 
 admin.site.register(Wine)
 admin.site.register(Collexn)
+admin.site.register(User)
--- winenot_backend/winenot_backend/settingsprod.py
+++ winenot_backend/winenot_backend/settingsprod.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=y5@_ddwbn74c4sh#7nccp5z0unn+1zjkmsx3(1y=&hq2@&0r)'
+# SECRET_KEY = 'django-insecure-=y5@_ddwbn74c4sh#7nccp5z0unn+1zjkmsx3(1y=&hq2@&0r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
