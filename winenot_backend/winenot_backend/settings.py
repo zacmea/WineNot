3b59efb6b393befbb101c7fc54e8047bffe83c41
zacmea <zach.mears@gmail.com>
2024-06-14 00:26:24 -0400

    trying to undo allowing removal of wine from list 9. sets debug=true

--- winenot_backend/winenot_backend/settings.py
+++ winenot_backend/winenot_backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=y5@_ddwbn74c4sh#7nccp5z0unn+1zjkmsx3(1y=&hq2@&0r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'api.winenot.com', 'wine-not.netlify.app', 'django-app-4ubj.onrender.com', 'dpg-cplloojgbbvc738qiae0-a', 'django-app-6z5w.onrender.com']
 
